4e9080790f8ffdba88672f4f530e9ee452efdae0
k2gutierrez <cchimal.gutierrez@gmail.com>
2022-07-06 09:48:04 -0500

    crud file change

--- crud.py
+++ crud.py
@@ -2,20 +2,21 @@ from flask import Flask, render_template, request, redirect, url_for, session
 import pyrebase
 import os
 from datetime import date
+import config
 
 app = Flask(__name__)
 
 app.secret_key = os.urandom(24)
 
 firebaseConfig = {
-  'apiKey': "AIzaSyD5mRUV08gkotpiTMoigFJd9Bfb9O8_hgQ",
-  'authDomain': "cedem-db.firebaseapp.com",
-  'databaseURL': "https://cedem-db-default-rtdb.firebaseio.com/",
-  'projectId': "cedem-db",
-  'storageBucket': "cedem-db.appspot.com",
-  'messagingSenderId': "272607335771",
-  'appId': "1:272607335771:web:dff8951eb8e08f8eef367e",
-  'measurementId': "G-ZT3V8MY4NC"
+  'apiKey': config.apiKey,
+  'authDomain': config.authDomain,
+  'databaseURL': config.databaseURL,
+  'projectId': config.projectId,
+  'storageBucket': config.storageBucket,
+  'messagingSenderId': config.messagingSenderId,
+  'appId': config.appId,
+  'measurementId': config.measurementId
   }
 
 #Firebase
