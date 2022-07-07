91489b8b00662b9fd82784ca3397ecf552b61041
k2gutierrez <cchimal.gutierrez@gmail.com>
2022-07-07 11:46:29 -0500

    se agregó sucesión y dianóstico de internacionalización

--- app.py
+++ app.py
@@ -2087,6 +2087,177 @@ def radararmonia():
     r3=r3, r4=r4, r5=r5, r6=r6, r7=r7, r8=r8, labels=labels, values=values)
 
 ###############################################################################################
+@app.route('/sucesion', methods= ['POST', 'GET'])
+def sucesion():
+
+    token = session['user']
+    user = auth.get_account_info(token)
+    localId = user['users'][0]['localId']
+    n = str(db.child(localId).child('NAME').get().val())
+    nombre = n.title()
+    mcont = {}
+    r1 = ''
+    r2 = ''
+    r3 = ''
+    r4 = ''
+    r5 = ''
+    r6 = ''
+    r7 = ''
+    mensaje = ''
+    hoy = date.today()
+
+    r1 = db.child(localId).child('MASTER').child("sucesion").child('r1').get().val()
+    r2 = db.child(localId).child('MASTER').child("sucesion").child('r2').get().val()
+    r3 = db.child(localId).child('MASTER').child("sucesion").child('r3').get().val()
+    r4 = db.child(localId).child('MASTER').child("sucesion").child('r4').get().val()
+    r5 = db.child(localId).child('MASTER').child("sucesion").child('r5').get().val()
+    r6 = db.child(localId).child('MASTER').child("sucesion").child('r6').get().val()
+    r7 = db.child(localId).child('MASTER').child("sucesion").child('r7').get().val()
+    fecha = db.child(localId).child('MASTER').child("sucesion").child('fecha').get().val()
+    
+    if r1 is None:
+        r1 = ''
+    
+    if r2 is None:
+        r2 = ''
+
+    if r3 is None:
+        r3 = ''
+    
+    if r4 is None:
+        r4 = ''
+
+    if r5 is None:
+        r5 = ''
+    
+    if r6 is None:
+        r6 = ''
+
+    if r7 is None:
+        r7 = ''
+
+    if fecha is None:
+        fecha = hoy
+    else:
+        fecha
+
+    if request.method == 'POST':
+        r1 = request.form.get('r1')
+        r2 = request.form.get('r2')
+        r3 = request.form.get('r3')
+        r4 = request.form.get('r4')
+        r5 = request.form.get('r5')
+        r6 = request.form.get('r6')
+        r7 = request.form.get('r7')
+
+        mcont = {
+            "r1": r1,
+            "r2": r2,
+            "r3": r3,
+            "r4": r4,
+            "r5": r5,
+            "r6": r6,
+            "r7": r7,
+            "fecha": str(hoy)
+        }
+        
+        mc = db.child(localId).child('MASTER').child("sucesion").set(mcont)
+        mc
+        mensaje = 'Los registros han quedado guardados'
+
+    return render_template('sucesion.html', mcont=mcont, mensaje=mensaje, nombre=nombre, fecha=fecha, r1=r1, r2=r2, 
+    r3=r3, r4=r4, r5=r5, r6=r6, r7=r7)
+
+###############################################################################################
+@app.route('/diagnosticointernacionalizacion', methods= ['POST', 'GET'])
+def diagnosticointernacionalizacion():
+
+    token = session['user']
+    user = auth.get_account_info(token)
+    localId = user['users'][0]['localId']
+    n = str(db.child(localId).child('NAME').get().val())
+    nombre = n.title()
+    mcont = {}
+    r1 = ''
+    r2 = ''
+    r3 = ''
+    r4 = ''
+    r5 = ''
+    r6 = ''
+    r7 = ''
+    r8 = ''
+    mensaje = ''
+    hoy = date.today()
+
+    r1 = db.child(localId).child('MASTER').child("diagnostico del proceso de internacionalizacion").child('r1').get().val()
+    r2 = db.child(localId).child('MASTER').child("diagnostico del proceso de internacionalizacion").child('r2').get().val()
+    r3 = db.child(localId).child('MASTER').child("diagnostico del proceso de internacionalizacion").child('r3').get().val()
+    r4 = db.child(localId).child('MASTER').child("diagnostico del proceso de internacionalizacion").child('r4').get().val()
+    r5 = db.child(localId).child('MASTER').child("diagnostico del proceso de internacionalizacion").child('r5').get().val()
+    r6 = db.child(localId).child('MASTER').child("diagnostico del proceso de internacionalizacion").child('r6').get().val()
+    r7 = db.child(localId).child('MASTER').child("diagnostico del proceso de internacionalizacion").child('r7').get().val()
+    r8 = db.child(localId).child('MASTER').child("diagnostico del proceso de internacionalizacion").child('r8').get().val()
+    fecha = db.child(localId).child('MASTER').child("diagnostico del proceso de internacionalizacion").child('fecha').get().val()
+    
+    if r1 is None:
+        r1 = ''
+    
+    if r2 is None:
+        r2 = ''
+
+    if r3 is None:
+        r3 = ''
+    
+    if r4 is None:
+        r4 = ''
+
+    if r5 is None:
+        r5 = ''
+    
+    if r6 is None:
+        r6 = ''
+
+    if r7 is None:
+        r7 = ''
+
+    if r8 is None:
+        r8 = ''
+
+    if fecha is None:
+        fecha = hoy
+    else:
+        fecha
+
+    if request.method == 'POST':
+        r1 = request.form.get('r1')
+        r2 = request.form.get('r2')
+        r3 = request.form.get('r3')
+        r4 = request.form.get('r4')
+        r5 = request.form.get('r5')
+        r6 = request.form.get('r6')
+        r7 = request.form.get('r7')
+        r8 = request.form.get('r8')
+
+        mcont = {
+            "r1": r1,
+            "r2": r2,
+            "r3": r3,
+            "r4": r4,
+            "r5": r5,
+            "r6": r6,
+            "r7": r7,
+            "r8": r8,
+            "fecha": str(hoy)
+        }
+        
+        mc = db.child(localId).child('MASTER').child("diagnostico del proceso de internacionalizacion").set(mcont)
+        mc
+        mensaje = 'Los registros han quedado guardados'
+
+    return render_template('diagnosticointernacionalizacion.html', mcont=mcont, mensaje=mensaje, nombre=nombre, fecha=fecha, r1=r1, r2=r2, 
+    r3=r3, r4=r4, r5=r5, r6=r6, r7=r7, r8=r8)
+
+###############################################################################################
 
 
 
